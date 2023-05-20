a303f9436f84e0908053d9f336fd073e104b4798
carvin <mobinanooshi2003@gmail.com>
2023-05-20 14:36:35 +0430

    algorithm 1

---
+++ A1.py
@@ -0,0 +1,6 @@
+def limit (arr , min=None ,max=None) :
+    min_check = lambda val : True if min is None else (val >= min)
+    max_check = lambda val : True if max is None else (val <= max)
+    
+    return [ val for val in arr if min_check(val) and max_check(val)]
+print(limit([1,47,5,8,74,55,25],max=20))
