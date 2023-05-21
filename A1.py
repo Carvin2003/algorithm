fde8bdc964837d9cd6545f17f34c941e875384fb
carvin <mobinanooshi2003@gmail.com>
2023-05-21 18:32:40 +0430

    algorithm 13

--- A1.py
+++ A1.py
@@ -178,16 +178,25 @@
 # print(k)
 # print(OneTime().decrypt(c, k))
 ###############################################################################
-def two_sum(numbers , target) :
-    p1 = 0
-    p2 = len(numbers)-1
-    while p1<=p2 :
-        s = numbers[p1] + numbers[p2]
-        if s==target :
-            return [p1 +1, p2+1]
-        elif s > target :
-            p2 =p2-1
-        else :
-            p1 = p1 + 1
-print(two_sum([2,7,11,15], 17))        
+# def two_sum(numbers , target) :
+#     p1 = 0
+#     p2 = len(numbers)-1
+#     while p1<=p2 :
+#         s = numbers[p1] + numbers[p2]
+#         if s==target :
+#             return [p1 +1, p2+1]
+#         elif s > target :
+#             p2 =p2-1
+#         else :
+#             p1 = p1 + 1
+# print(two_sum([2,7,11,15], 17))   
+###############################################################################
+def rotate(s, k) :
+    double_s = s + s
+    if k < len(s) :
+        return double_s[k:k+len(s)]
+    else :
+        return double_s[k-len(s):k]
+    
+print(rotate('mobin', 2))
     
