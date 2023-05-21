e49999d0d06cf0f1e1f49d812ac9c56a79a87af6
carvin <mobinanooshi2003@gmail.com>
2023-05-21 07:50:07 +0430

    algorithm 11

--- A1.py
+++ A1.py
@@ -135,22 +135,48 @@
 #     return result
 # print(move_zero([False,1,0,2,0,1,1,0,1,0,0,"a"]))
 ###############################################################################
-def remove_min(stack) :
-    storage_stack =[]
-    if len(stack) ==0 :
-        return stack
-    min = stack.pop()
-    stack.append(min)
-    for i in range(len(stack)):
-        val =stack.pop()
-        if val <= min :
-            min = val
-        storage_stack.append(val)
-    for i in range(len(storage_stack)) :
-        val = storage_stack.pop()
-        if val !=min :
-            stack.append(val)
-    return stack ,min
-print(remove_min([4,5,2,8,-2,5,1,8,9]))
+# def remove_min(stack) :
+#     storage_stack =[]
+#     if len(stack) ==0 :
+#         return stack
+#     min = stack.pop()
+#     stack.append(min)
+#     for i in range(len(stack)):
+#         val =stack.pop()
+#         if val <= min :
+#             min = val
+#         storage_stack.append(val)
+#     for i in range(len(storage_stack)) :
+#         val = storage_stack.pop()
+#         if val !=min :
+#             stack.append(val)
+#     return stack ,min
+# print(remove_min([4,5,2,8,-2,5,1,8,9]))
+###############################################################################
+import random
+class OneTime :
+    def encrypt(self ,text) :
+        plain = [ord(i) for i in text]
+        key = []
+        cipher = []
+        for i in plain :
+            k = random.randint(1, 1000)
+            c = (i + k)*k
+            cipher.append(c)
+            key.append(k)
+        return cipher , key
+    def decrypt(self , cipher, key) :
+        plain = []
+        for i in range(len(key)):
+            p= int((cipher[i] -key[i] ** 2) / key[i])
+            plain.append(chr(p))
+        result = ''.join([i for i in plain])
+        return result
+    
+c,k = OneTime().encrypt('mobin')
+print(c)
+print(k)
+print(OneTime().decrypt(c, k))
+
         
     
